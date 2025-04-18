bc2eefbb60271881378a0fab86ca6d88e28e4889
sampurna <sampurnachakraborty798@gmail.com>
2025-04-18 16:08:39 +0530

    object created

---
+++ js/project.js
@@ -0,0 +1,14 @@
+var x ,y ,sum;
+
+
+x = parseInt(prompt ('Enter First Number'))
+y = parseInt(prompt ('Enter Second Number'))
+
+//x = parseFloat(prompt ('Enter First Number'))
+//y = parseFloat(prompt ('Enter Second Number'))
+
+sum = x+y;
+
+document.write('Total Number = ' +sum)
+
+
