5ae7486f43a0241fbaef70853e8273a227c9f23d
sampurna <sampurnachakraborty798@gmail.com>
2025-04-19 14:01:31 +0530

    condition applied

--- js/project.js
+++ js/project.js
@@ -1,14 +1,26 @@
-var x ,y ,sum;
 
 
-x = parseInt(prompt ('Enter First Number'))
-y = parseInt(prompt ('Enter Second Number'))
 
-//x = parseFloat(prompt ('Enter First Number'))
-//y = parseFloat(prompt ('Enter Second Number'))
+let subject = 'English';
 
-sum = x+y;
+if(subject == 'English') {
+    document.write("Subject is English")
+}
+else if(subject == 'Bengali') {
+    document.write("Subject is Bengali")
+}
+else if(subject == 'Hindi') {
+    document.write("Subject is Hindi")
+}
+else if(subject == 'Math') {
+    document.write("Subject is Math")
+}
+else if(subject == 'Biology') {
+    document.write("Subject is Biology")
+}
+else {
+    document.write('Subject is Invalid')
+}
 
-document.write('Total Number = ' +sum)
 
 
